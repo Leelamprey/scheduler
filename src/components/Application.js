2a2090f9baf4530a598878cbc14e44fd7a058b71
Lee Benzacar <lmbenzacar@gmail.com>
2022-03-01 21:07:38 -0500

    major update, forgot to commit for a while

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 import "components/Application.scss";
 
+// Fake data
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm"
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Théo MLP",
+      interviewer: { 
+        id: 3, 
+        name: "Mildred Nazir", 
+        avatar: "https://i.imgur.com/T2WwVfS.png" }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Julien",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+];
+
 export default function Application(props) {
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
       <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu"></nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
-
+        className="sidebar--centered"
+        src="images/logo.png"
+        alt="Interview Scheduler"
+      />
+      <hr className="sidebar__separator sidebar--centered" />
+      <nav className="sidebar__menu">
+        <DayList 
+          days={days} 
+          day={day} 
+          setDay={setDay} 
+        />
+      </nav>
+      
+      <img
+        className="sidebar__lhl sidebar--centered"
+        src="images/lhl.png"
+        alt="Lighthouse Labs"
+      />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(a => <Appointment key={a.id} {...a} />)}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
