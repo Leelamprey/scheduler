beb5eb018a6b225def668469fe977cda75206f1e
Lee Benzacar <lmbenzacar@gmail.com>
2022-03-06 12:10:58 -0500

    Added getInterview selector and formatted it

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
-import getAppointmentsForDay from "../helpers/selectors";
-
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 import "components/Application.scss";
 
 export default function Application(props) {
@@ -15,11 +14,9 @@ export default function Application(props) {
 
   const setDay = day => setState({ ...state, day});
 
-
   useEffect(() => {
     const promiseDays = axios.get("/api/days");
     const promiseAppointments = axios.get("/api/appointments");
-
     Promise.all([promiseDays, promiseAppointments])
       .then(all => {
         setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }))
